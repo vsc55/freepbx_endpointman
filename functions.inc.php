64b5118573efb869c0bcfa8cdf42261721734afa
gwatkins1957 <Greg@DSSI.local>
2016-03-16 11:44:10 -0500

    Fix for grandstream reset
    Changed grandstream reset function from sys-control to check-sync.

--- functions.inc.php
+++ functions.inc.php
@@ -18,7 +18,7 @@ function endpointman_get_config($engine) {
                 $core_conf->addSipNotify('polycom-check-cfg', array('Event' => 'check-sync', 'Content-Length' => '0'));
                 $core_conf->addSipNotify('polycom-reboot', array('Event' => 'check-sync', 'Content-Length' => '0'));
                 $core_conf->addSipNotify('sipura-check-cfg', array('Event' => 'resync', 'Content-Length' => '0'));
-                $core_conf->addSipNotify('grandstream-check-cfg', array('Event' => 'sys-control'));
+                $core_conf->addSipNotify('grandstream-check-cfg', array('Event' => 'check-sync'));
                 $core_conf->addSipNotify('cisco-check-cfg', array('Event' => 'check-sync', 'Content-Length' => '0'));
                 $core_conf->addSipNotify('reboot-snom', array('Event' => 'reboot', 'Content-Length' => '0'));
                 $core_conf->addSipNotify('aastra-check-cfg', array('Event' => 'check-sync', 'Content-Length' => '0'));
